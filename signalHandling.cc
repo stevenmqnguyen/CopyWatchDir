73f73227a31a7123ec998d960c1c15c5ff2178fb
Steven Nguyen <sqn170000@cslinux2.utdallas.edu>
2021-05-08 18:43:40 -0500

    add verbose logging

--- signalHandling.cc
+++ signalHandling.cc
@@ -21,6 +21,14 @@ void signalHandler( int signum ){
         g_config = reparseConfig();
         if(g_optionMap[verbose].compare("true") == 0){
             g_logfile << "Reparsed config file due to signal: SIGHUP" << endl;
+            g_logfile << "daemon_value: " << g_optionMap[daemon_value];
+            g_logfile << "config_filename: " << g_optionMap[config_filename];
+            g_logfile << "verbose: " << g_optionMap[verbose];
+            g_logfile << "logfile_filename: " << g_optionMap[logfile_filename];
+            g_logfile << "password: " << g_optionMap[password];
+            g_logfile << "numVersions: " << g_optionMap[numVersions];
+            g_logfile << "watchdir: " << g_optionMap[watchdir];
+            //daemon_value, config_filename, verbose, logfile_filename, password, numVersions, watchdir
         }
     }
 }
