5f2d5ecd44077cdc9971de8a0d27d1128937bd3f
Steven Nguyen <sqn170000@cslinux2.utdallas.edu>
2021-05-08 18:16:59 -0500

    Add signal handling

--- signalHandling.cc
+++ signalHandling.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <csignal> 
 #include <stdlib.h>
+#include "enum.h"
 #include "globals.h"
 #include "parseConfigFile.h"
 
@@ -9,12 +10,17 @@ using namespace std;
 void signalHandler( int signum ){
     if(signum == SIGINT || signum == SIGTERM){
         g_pidfile.close();
-        g_logfile << "Closed due to signal: " << (signum == SIGINT ? "SIGINT" : "SIGTERM") << endl;
+        if(g_optionMap[verbose].compare("true") == 0){
+            g_logfile << "Closed due to signal: " << (signum == SIGINT ? "SIGINT" : "SIGTERM") << endl;
+        }
         g_logfile.close();
         system("rm cs3377dirmond.pid");
         exit(0); 
     }
     else if(signum == SIGHUP){
         g_config = reparseConfig();
+        if(g_optionMap[verbose].compare("true") == 0){
+            g_logfile << "Reparsed config file due to signal: SIGHUP" << endl;
+        }
     }
 }
