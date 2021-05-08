ad86727bbc6fe271ff9ca8282a06bede14d83d9e
Steven Nguyen <sqn170000@cslinux1.utdallas.edu>
2021-05-08 03:32:55 -0500

    abstract process control

--- main.cc
+++ main.cc
@@ -12,6 +12,7 @@
 #include "parseCommandLine.h"
 #include "parseConfigFile.h"
 #include "loggingFunctions.h"
+#include "processControl.h"
 
 using namespace std;
 using namespace rude;
@@ -26,40 +27,40 @@ int main(int argc, char* argv[])
 	Config config = parseConfig(optionMap);
 	
 	// run program with fork
-	if(optionMap[daemon_value].compare("true") == 0){
-		pid_t forkvalue = fork();
+	// if(optionMap[daemon_value].compare("true") == 0){
+	// 	pid_t forkvalue = fork();
 
-		// Error, no child created
-		if(forkvalue == -1){
-			cerr << "Error in fork. No child created." << endl;
-			exit(1);
-		}
-		// Child process
-		else if(forkvalue == 0){
-			cout << "This is the child process running!" << endl;
-			if(PIDFileExists("cs3377dirmond.pid")){
-				exit(1);
-			}
-			ofstream pidfile("cs3377dirmond.pid");
-			pidfile << getpid() << endl;
-			int i = 1;
-			while(true){
-				i++;
-			}
-		}
-		// Parent process
-		else{
-			cout << "This is the parent process running! Child PID: " << forkvalue << endl;
-			exit(0);
-		}
+	// 	// Error, no child created
+	// 	if(forkvalue == -1){
+	// 		cerr << "Error in fork. No child created." << endl;
+	// 		exit(1);
+	// 	}
+	// 	// Child process
+	// 	else if(forkvalue == 0){
+	// 		cout << "This is the child process running!" << endl;
+	// 		if(PIDFileExists("cs3377dirmond.pid")){
+	// 			exit(1);
+	// 		}
+	// 		ofstream pidfile("cs3377dirmond.pid");
+	// 		pidfile << getpid() << endl;
+	// 		int i = 1;
+	// 		while(true){
+	// 			i++;
+	// 		}
+	// 	}
+	// 	// Parent process
+	// 	else{
+	// 		cout << "This is the parent process running! Child PID: " << forkvalue << endl;
+	// 		exit(0);
+	// 	}
 
-	}
-	// Run program non daemon style (no forking)
-	else{
-		cout << "This is running without any daemons" << endl;
+	// }
+	// // Run program non daemon style (no forking)
+	// else{
+	// 	cout << "This is running without any daemons" << endl;
 		
-	}
-
+	// }
+	processControl(optionMap);
 
     return 0;
 }
