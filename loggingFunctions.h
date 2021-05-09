98d0df82836f37dabaada1a31740a29b2714b8c6
Steven Nguyen <sqn170000@cslinux2.utdallas.edu>
2021-05-09 04:14:20 -0500

    Add file header

--- loggingFunctions.h
+++ loggingFunctions.h
@@ -1,3 +1,19 @@
+/*
+ * Filename	loggingFunctions.h
+ * Date 	May 9, 2021
+ * Author	Steven Nguyen
+ * Email	steven.nguyen@utdallas.edu
+ * Course	CS 3377.0W5 Spring 2021
+ * Version 	1.0
+ * Copyright	2021, All Rights Reserved
+ *
+ * Description
+ * 	Daemon process that will watch a folder and respond to specific file operations that occur in that folder.
+ *	The daemon will read its configuration parameters from a CONFIG file and then operate according to that configuration.
+ *  When it detects a write, the daemon will immediately create a copy of the just written file and store that copy in a
+ *  .versions subfolder. Each copy stored in the .versions subfolder will have a date stamp appended to the filename.
+ *
+ */
 #ifndef __LOGGINGFUNCTIONS__
 #define __LOGGINGFUNCTIONS__
 
